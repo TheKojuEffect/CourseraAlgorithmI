34f90b707f0a1be85930e81d99000414f22d00e7
Kapil Koju <kplx2.0@gmail.com>
2012-08-23 12:34:18 +0545

    UnionFind Implemented

--- CourseraAlgorithmI/src/unionfind/QuickFind.java
+++ CourseraAlgorithmI/src/unionfind/QuickFind.java
@@ -1,23 +1,25 @@
 package unionfind;
 
-public class QuickFind {
-
+public class QuickFind implements UnionFind {
+	
 	private int[]	id;
-
+	
 	public QuickFind(int number) {
 		id = new int[number];
 		for (int i = 0; i < id.length; i++)
 			id[i] = i;
 	}
-
+	
+	@Override
 	public boolean connected(int p, int q) {
 		return id[p] == id[q];
 	} // end method connected
-
+	
+	@Override
 	public void union(int p, int q) {
 		if (id[p] == id[q])
 			return;
-
+		
 		int pid = id[p];
 		int qid = id[q];
 		for (int i = 0; i < id.length; i++)
