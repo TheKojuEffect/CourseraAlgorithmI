8fb472ce9827d7a5840d583764bf7f224083683b
Kapil Koju <kplx2.0@gmail.com>
2012-08-23 19:51:46 +0545

    Initialized Percolation Classes
    	new file:   CourseraAlgorithmI/src/unionfind/Percolation.java
    	new file:   CourseraAlgorithmI/src/unionfind/PercolationStats.java

---
+++ CourseraAlgorithmI/src/unionfind/Percolation.java
@@ -0,0 +1,27 @@
+package unionfind;
+
+public class Percolation {
+	
+	public Percolation(int number) {
+		
+	}
+	
+	public void open(int i, int j) {
+		
+	} // end method open
+	
+	public boolean isOpen(int i, int j) {
+		return false;
+		
+	} // end method isOpen
+	
+	public boolean isFull(int i, int j) {
+		return false;
+		
+	} // end method isFull
+	
+	public boolean percolates() {
+		return false;
+		
+	} // end method percolates
+} // end class Percolation
